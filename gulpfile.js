b057e9638380ec2ec7909331df9632b2e7a599af
Desarrollo Ondigital <ondigital.desarrollo@gmail.com>
2017-02-08 14:38:25 -0500

    Automatizacion de procesos front-end para el manejo de tareas

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,18 @@
 const gulp = require('gulp'),
 	  sass = require('gulp-sass'),
 	  pug  = require('gulp-pug'),
-	  autoprefixer = require('gulp-autoprefixer');
+	  autoprefixer = require('gulp-autoprefixer'),
+	  browserSync = require('browser-sync').create();
+
+// Navegador
+
+gulp.task('navegador', () => {
+	browserSync.init({
+		server: './dist'
+	})
+	gulp.watch('./dist/*.html').on('change', browserSync.reload);
+	gulp.watch('./css/*.html').on('change', browserSync.reload);
+});
 
 // Compilar sass
 gulp.task('sass', () => {
@@ -26,9 +37,11 @@ gulp.task('pug', () => {
 });
 
 // Compilacion Total del Proyecto
-gulp.task('default',['sass','pug'], () => {
+gulp.task('default',['sass','pug','navegador'], (event) => {
 	gulp.watch('./scss/**/*.scss', ['sass']);
 	gulp.watch('./dev/views/**/*.pug', ['pug']);
+	  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+
 });
 
 // Observar el proyecto 
